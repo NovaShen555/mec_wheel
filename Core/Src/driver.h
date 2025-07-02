6b89890180a6494b9cd80619cdcf30560b0f3ce7
NovaShen555 <admin@novashen.top>
2025-07-02 19:45:20 +0800

    Initial commit

---
+++ Core/Src/driver.h
@@ -0,0 +1,17 @@
+//
+// Created by 10415 on 25-7-2.
+//
+
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#define MOTOR_A 1
+#define MOTOR_B 2
+#define MOTOR_C 3
+#define MOTOR_D 4
+
+void SetMotor(int motor, int speed);
+
+void SetPolarity(int motor, int polarity);
+
+#endif //DRIVER_H
